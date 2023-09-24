f466cb78b4161e718da5cf66246f77ca5d5498e3
marielaveron <maravsf@gmail.com>
2023-09-24 15:05:24 -0300

    Subiendo ejercicio

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function enviarDatos() {
+    let name = document.getElementById('nombre').value;
+    let lastName = document.getElementById('apellido').value;
+    let date = document.getElementById('fecha').value;
+
+    fetch('https://jsonplaceholder.typicode.com/users', {
+        headers: { "Content-Type": "application/json; charset=utf-8" },
+        method: 'POST',
+        body: JSON.stringify({
+            nombre: name,
+            apellido: lastName,
+            fechaNac: date
+        })
+    })
+    .then(response => response.json())
+    .then(datos => {
+        document.getElementById('nombre').value = "";
+        document.getElementById('apellido').value = "";
+        document.getElementById('fecha').value = 0;
+        alert("Enviado correctamente")
+    })
+}
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('enviar').addEventListener('click', ()=> {
+        enviarDatos();
+    })
+})
